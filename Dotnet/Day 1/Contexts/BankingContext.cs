8fc3be57a3d7069f9d111448dd2f498567f7caec
Sri Manikandan R <srimanikandandev@gmail.com>
2026-05-13 14:56:00 +0530

    made changes to the entity context

--- Dotnet/Day 1/Contexts/BankingContext.cs
+++ Dotnet/Day 1/Contexts/BankingContext.cs
@@ -12,10 +12,46 @@ namespace UnderstandingEfCoreApp.Contexts
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=bankingdb;Username=postgres;Password=Poornima290178@");
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=bankingdb;Username=postgres;Password=password");
         }
 
-        public DbSet<Customer>  customers { get; set; }
+        public DbSet<Customer> customers { get; set; }
         public DbSet<Account> accounts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>(c =>
+            {
+                c.HasKey(c => c.Id);
+                c.Property(c => c.DateOfBirth).HasColumnType("timestamp without time zone");
+                //seeding
+                c.HasData(new Customer() { Id = 101, Name = "Ramu", DateOfBirth = DateTime.Now, Phone = "9876543210", Email = "ramu@gmail.com", Status = "Active" });
+            });
+
+
+
+            modelBuilder.Entity<Account>(a =>
+            {
+                a.HasKey(a => a.AccountNumber);
+
+                a.HasOne(a => a.Customer)
+                .WithMany(c => c.Accounts)
+                .HasForeignKey(a => a.CustomerId)
+                .HasConstraintName("FK_Account_Customer")
+                .OnDelete(DeleteBehavior.Restrict);
+
+                a.Property(a => a.LastAccessed).HasColumnType("timestamp without time zone");
+
+                a.HasData(new Account()
+                {
+                    AccountNumber = "0009998877",
+                    Balance = 134.3M,
+                    CustomerId = 101,
+                    LastAccessed = DateTime.Now,
+                    Status = "Active"
+                });
+            });
+
+        }
     }
 }
